e392c1c8180cd75af5984cc50fc77a48a79a139c
komisuomi <only-for-serzh@yandex.ru>
2020-11-18 14:11:29 +0300

    JUnit4 Added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,27 +1,27 @@
 package ru.netology.service;
 
-import static org.testng.AssertJUnit.*;
-import org.testng.annotations.Test;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
-    void shouldAdviseToBuyMore() {
+    public void shouldAdviseToBuyMore() {
         int amount = 900;
 
         int actual = service.remain(amount);
         int expected = 100;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void shouldNotAdviseToBuyMore() {
+    public void shouldNotAdviseToBuyMore() {
         int amount = 1000;
 
         int actual = service.remain(amount);
         int expected = 0;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
